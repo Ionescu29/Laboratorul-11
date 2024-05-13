1634719a9a604dfc1c1075a431148c1b7918eaff
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-05-13 18:41:59 +0200

    Add files via upload

---
+++ Lab11Ex1/Lab11Ex1/Autoturism/Autovehicul.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11Ex1.Autoturism
+{
+    //Autovehiculele sunt caracterizate de model, id si o metoda ce va produce descrierea completa a
+    //autovehiculului.Ele pot fi de mai multe tipuri
+    internal class Autovehicul
+    {
+        private string id;
+        private string model;
+        private string tipulDeCombustibil;
+        private int capacitateaRezervorului;
+        private string combustibilIncarcat;
+        private int cantitate;
+        public Autovehicul(string id, string model, string tipulDeCombustibil, int capacitateaRezervorului, string combustibilIncarat, int cantitate)
+        {
+            this.id = id;
+            this.model = model;
+            this.tipulDeCombustibil = tipulDeCombustibil;
+            this.capacitateaRezervorului = capacitateaRezervorului;
+            this.combustibilIncarcat = combustibilIncarcat;
+            this.cantitate = cantitate;
+        }
+        public void GetDescrpition()
+        {
+            Console.WriteLine(id , model, tipulDeCombustibil);
+        }
+
+        //Scrieti o metoda pentru alimentarea unui autovehicul
+        //Metoda va primi ca parametru cantiatatea si tipul combustibilului incarcat
+        //Aceasta metoda va fi apelata de catre cisterna, in procesul de alimentare
+        //Un autovehicul nu va putea fi alimentat peste capacitatea maxima a rezervorului acestuia.
+        //Un autovehicul nu va putea fi alimentat cu un combustibil incompatibil
+        public void AlimentareVehicul()
+        {
+            if(tipulDeCombustibil==combustibilIncarcat)
+            {
+                Console.WriteLine($"Masina poate fi alimentata");
+            }
+            else
+            {
+                Console.WriteLine($"Masina nu poate fi alimentata");
+            }
+            if(cantitate>capacitateaRezervorului)
+            {
+                Console.WriteLine($"Masina nu poate fi alimentata");
+            }
+            else
+            {
+                Console.WriteLine($"Masina  poate fi aliemntata");
+            }
+        }
+    }
+}
---
+++ Lab11Ex1/Lab11Ex1/Cisterna/Cisterna.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11Ex1.Cisterna
+{
+    //O cisterna poate pune la dispozitia clientilor mai multe tipuri de combustibil (benzina, diesel, GPL, electricitate)
+    internal class Cisterna
+    {
+        private string tipulCombustibilului;
+        private int cantitateaDeCombustibil;
+        private string tipulCombustibilului;
+        private int capacitateaRezervorului;
+        private string combustibilIncarcat;
+        public Cisterna (string tipulCombustibilului, int cantitateaDeCombustibil, string tipulCombustibilului, int capacitateaRezervorului, string combustibilIncarcat)
+        {
+            this.tipulCombustibilului = tipulCombustibilului;
+            this.cantitateaDeCombustibil = cantitateaDeCombustibil;
+            this.tipulCombustibilului = tipulCombustibilului;
+            this.capacitateaRezervorului = capacitateaRezervorului;
+            this.combustibilIncarcat = combustibilIncarcat;
+        }
+        //Scrieti o metoda pentru incarcare, care va primi ca parametri cantitatea pentru incarcat precum
+        //si tipul combustibilului incarcat
+        //Cisterna nu poate fi incarcata peste capacitatea maxima.
+        //Cisterna nu poate fi incarcata cu un combustibil diferit fata de cel deja incarcat.
+
+        public void Incarcarea()
+        {
+            if(cantitateaDeCombustibil>capacitateaRezervorului)
+            {
+                Console.WriteLine($"Cisterna nu poate fi incarcata");
+            }
+            else
+            {
+                Console.WriteLine($"Csterna poate fi incarcata");
+            }
+            if(tipulCombustibilului!=combustibilIncarcat)
+            {
+                Console.WriteLine($"Cisterna nu poate fi incarcata");
+            }
+        }
+        //Cisterna va putea alimenta orice autovehicul. Scrieti o metoda pentru incarcarea unui
+        //autovehicul, care va primi ca parametru o referinta spre autovehicul precum si cantitatea de
+        //combustibil dorita.
+        //Metoda cisternei va apela metoda „alimenteaza” a autovehiculului.
+        //Tratati situatiile in care incarcarea nu ar fi posibila din perspectiva cisternei.
+       
+    }
+}
